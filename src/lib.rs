b16776cd88e222120d5ef1a8a612d4a57bdb80e2
zenlex <erichrkeil@gmail.com>
2023-06-01 22:47:29 -0500

    --wip-- sketch in ssrjs stack

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,9 @@
 The basic idea for a given stack:
 - ask for project name
 - create a directory with slugified project name
+- init package (npm init, cargo init, etc)
 - install dependencies
+- install dev dependencies - testing, linting, formatting, etc
 - create a docker image from template file
 - copy/patch templates for that stack
 - git init
--- templates/ssrjs/astro.config.mjs
+++ templates/ssrjs/astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config'
+import vue from '@astrojs/vue'
 
 export default defineConfig({
-	output: 'server'
+	output: 'server',
+	integrations: [vue()],
 });
