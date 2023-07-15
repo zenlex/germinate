e34def88e15bbe8a049127988165ecc7793ee581
zenlex <erichrkeil@gmail.com>
2023-07-15 18:04:29 -0500

    feat: parse npm deps from toml first pass

--- src/module/mod.rs
+++ src/module/mod.rs
@@ -1,4 +1,4 @@
-use std::{fs, path::Path};
+use std::{collections::HashMap, fs, path::Path};
 use toml::Table;
 #[derive(Debug, Clone)]
 pub struct Module {
@@ -19,21 +19,85 @@ impl Module {
     }
 }
 
+type Dependencies = HashMap<String, Vec<Module>>;
+type Scripts = HashMap<String, Vec<String>>;
 struct TomlTemplate {
     title: String,
     subfolders: Option<Vec<Vec<String>>>,
-    scripts: Option<Vec<Vec<String>>>,
-    dependencies: Option<Vec<Vec<Module>>>,
+    scripts: Option<Scripts>,
+    dependencies: Option<Dependencies>,
+}
+
+impl TomlTemplate {
+    pub fn parse_deps(path: &Path) -> Option<Dependencies> {
+        let template_str = fs::read_to_string(path).expect("Error reading file");
+        let table = template_str.parse::<Table>().expect("Error parsing toml");
+        let deps = table["dependencies"]
+            .as_table()
+            .expect("Error parsing dependencies");
+
+        let npm_deps: Vec<_> = deps["npm"]
+            .as_array()
+            .expect("Error parsing npm dependencies")
+            .iter()
+            .map(|dep| {
+                let dep = dep.as_table().expect("Error parsing dep");
+                let name = dep["name"].as_str().expect("Error parsing name");
+
+                //TODO: add semver crate to allow for parsing semver ranges
+                let version = if dep.contains_key("version") {
+                    dep["version"].as_str().expect("Error parsing version")
+                } else {
+                    "latest"
+                };
+
+                let dev = if dep.contains_key("dev") {
+                    dep["dev"].as_bool().expect("Error parsing dev")
+                } else {
+                    false
+                };
+
+                let then = if dep.contains_key("then") {
+                    Some(
+                        dep["then"]
+                            .as_array()
+                            .unwrap_or(&toml::value::Array::new())
+                            .iter()
+                            .map(|then| then.to_string())
+                            .collect(),
+                    )
+                } else {
+                    None
+                };
+                Module::new(name.to_string(), version.to_string(), dev, then)
+            })
+            .collect();
+
+        let mut deps = HashMap::new();
+        deps.insert("npm".to_string(), npm_deps);
+        Some(deps)
+    }
 }
 
 pub struct NpmDeps {
     deps: Vec<Module>,
 }
 
-// read toml file to string
-// parse toml string to table
-// create module structs for each dep
-// construct TomlTemplate struct from table
+impl NpmDeps {
+    pub fn new() -> Self {
+        Self { deps: Vec::new() }
+    }
+
+    pub fn add(&mut self, module: Module) {
+        self.deps.push(module);
+    }
+
+    pub fn get(&self) -> Vec<Module> {
+        self.deps.clone()
+    }
+}
+
+// TODO add builders for composer and cargo deps
 // ? refactor TomlTemplate construction to deserialize traits for toml? (maybe not worth it)
 #[cfg(test)]
 pub mod tests {
@@ -46,6 +110,13 @@ pub mod tests {
         let template_str = fs::read_to_string(path).expect("Error reading file");
         let table = template_str.parse::<Table>().expect("Error parsing toml");
         assert_eq!(table["title"].as_str(), Some("ssrjs"));
-        println!("{:?}", table);
+    }
+
+    #[test]
+    fn extract_npm_deps() {
+        //TODO: make a test template file and assert the results
+        let path = Path::new("templates/ssrjs/ssrjs.toml");
+        let parsed_deps = TomlTemplate::parse_deps(path).expect("Error parsing deps");
+        assert_eq!(parsed_deps.contains_key("npm"), true);
     }
 }
