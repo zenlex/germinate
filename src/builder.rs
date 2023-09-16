6009a138093fd9cc7ff5bac7f8370e9aca1eaadc
zenlex <erichrkeil@gmail.com>
2023-09-16 18:18:35 -0500

    feat: add handlebars templating for dockerfile

--- src/builder.rs
+++ src/builder.rs
@@ -2,10 +2,14 @@ use crate::{config::ScaffoldConfig, dialogue::StackTemplate, file_system, module
 use std::{
     env,
     fmt::{self, Debug, Formatter},
+    fs,
     path::PathBuf,
     process::Command,
     vec,
 };
+
+use handlebars::Handlebars;
+use serde::{Deserialize, Serialize};
 
 pub struct ProjectBuilder {
     config: ScaffoldConfig,
@@ -29,6 +33,10 @@ impl ProjectBuilder {
             let output = command.output().expect("Failed to execute command");
             println!("->> STDOUT: {}", String::from_utf8_lossy(&output.stdout));
             println!("->> STDERR: {}", String::from_utf8_lossy(&output.stderr));
+        }
+
+        if self.config.containers {
+            self.copy_docker_files();
         }
 
         self.set_npm_scripts();
@@ -258,6 +266,36 @@ impl ProjectBuilder {
             .expect("unable to copy dir");
     }
 
+    fn copy_docker_files(&self) {
+        self.generate_dockerfile();
+        println!("Copying Docker files...");
+        let pre_install_path = self.template_dir().join("docker");
+        file_system::copy_dir_all(pre_install_path, env::current_dir().unwrap())
+            .expect("unable to copy dir");
+
+        println!("Removing templates...");
+        fs::remove_file("dockerfile.template").expect("Failed to remove dockerfile template");
+    }
+
+    fn generate_dockerfile(&self) {
+        println!("Generating Docker files...");
+
+        let hb = Handlebars::new();
+        hb.render_template_to_write(
+            fs::read_to_string(
+                &self
+                    .template_dir()
+                    .join("docker")
+                    .join("dockerfile.template"),
+            )
+            .unwrap()
+            .as_str(),
+            &DockerVariables::new(),
+            &mut fs::File::create("Dockerfile").unwrap(),
+        )
+        .expect("Failed to render dockerfile template");
+    }
+
     fn template_dir(&self) -> PathBuf {
         env::current_exe()
             .unwrap()
@@ -272,5 +310,24 @@ impl Debug for ProjectBuilder {
         f.debug_struct("Project Builder")
             .field("config", &self.config)
             .finish()
+    }
+}
+//
+#[derive(Serialize, Deserialize, Debug)]
+struct DockerVariables {
+    app_name: String,
+}
+
+impl DockerVariables {
+    pub fn new() -> Self {
+        Self {
+            app_name: env::current_dir()
+                .unwrap()
+                .file_name()
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .to_string(),
+        }
     }
 }
