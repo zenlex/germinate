a1268a83b7f66e9eb666d6ba04ae5ab4a0622800
zenlex <erichrkeil@gmail.com>
2023-09-22 21:14:29 -0500

    feat: first pass rust web stack with docker

--- src/container.rs
+++ src/container.rs
@@ -4,7 +4,7 @@ use std::{
     fs::{self},
 };
 
-use crate::{config::ScaffoldConfig, file_system};
+use crate::{config::ScaffoldConfig, dialogue::Database, file_system};
 
 pub struct ContainerBuilder {
     config: ScaffoldConfig,
@@ -25,11 +25,12 @@ impl ContainerBuilder {
 #[derive(Serialize, Deserialize, Debug)]
 pub struct DockerVariables {
     app_name: String,
-    deps_name: String,
+    deps_name: String, // the app name prefix of the generated deps files to remove in prod for containers
+    postgres: bool,
 }
 
 impl DockerVariables {
-    pub fn new() -> Self {
+    pub fn new(db: &Option<Database>) -> Self {
         let kebab_name = env::current_dir()
             .unwrap()
             .file_name()
@@ -42,12 +43,14 @@ impl DockerVariables {
         Self {
             app_name: kebab_name,
             deps_name: snake_name,
+            postgres: matches!(db.as_ref().unwrap(), Database::Postgres),
         }
     }
 }
 
 fn copy_docker_files(config: &ScaffoldConfig) {
     generate_dockerfile(config);
+    generate_docker_compose(config);
     println!("Copying Docker files...");
     let pre_install_path = config.template_dir.join("docker");
     dbg!(&pre_install_path);
@@ -68,8 +71,25 @@ fn generate_dockerfile(config: &ScaffoldConfig) {
     let template = fs::read_to_string(template_path).expect("Failed to read dockerfile template");
     crate::template_generator::render_to_file(
         &template,
-        &DockerVariables::new(),
+        &DockerVariables::new(&config.db),
         &mut fs::File::create("Dockerfile").unwrap(),
+    )
+    .expect("Failed to render dockerfile template");
+}
+
+fn generate_docker_compose(config: &ScaffoldConfig) {
+    println!("Generating docker-compose files...");
+    let template_path = config
+        .template_dir
+        .join("docker")
+        .join("docker-compose.yml.template");
+
+    dbg!(&template_path);
+    let template = fs::read_to_string(template_path).expect("Failed to read dockerfile template");
+    crate::template_generator::render_to_file(
+        &template,
+        &DockerVariables::new(&config.db),
+        &mut fs::File::create("docker-compose.yml").unwrap(),
     )
     .expect("Failed to render dockerfile template");
 }
---
+++ templates/rsapi/after_install/src/main.rs
@@ -0,0 +1,13 @@
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    println!("Starting server on port 3000");
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+    println!("Server started");
+}
