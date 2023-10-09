3b633e0a0f73e4cff77e93dcee0ef56e2c881217
zenlex <erichrkeil@gmail.com>
2023-10-09 10:12:41 -0500

    feat: add env variables to build script

--- build.sh
+++ build.sh
@@ -1,1 +1,22 @@
-cargo build && rm -rf ~/clitools/germinate/templates && cp -p target/debug/germinate ~/clitools/germinate && cp -rf templates ~/clitools/germinate
+# /germinate/templates wll be created at this path
+TEMPLATE_PATH=~/clitools
+
+# use debug for development, release for production
+BUILD=debug # options: debug, release
+
+if [$BUILD eq release] 
+then 
+cargo build --release
+
+else
+cargo build
+fi
+
+echo "Building germinate in $BUILD mode" 
+
+echo "Removing old files from $TEMPLATE_PATH/germinate"
+rm -rf ${TEMPLATE_PATH}/germinate/templates 
+
+echo "Copying new files to $TEMPLATE_PATH/germinate"	
+cp -p target/debug/germinate ${TEMPLATE_PATH}/germinate 
+cp -rf templates ${TEMPLATE_PATH}/germinate
